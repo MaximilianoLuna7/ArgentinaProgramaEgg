bee40586c7717bdaa434017d9ff3b7c6ebb42c0d
Maximiliano Luna <maxiespn21@gmail.com>
2023-07-06 09:20:28 -0300

    Se realizó el ejercicio 1 de la guía de relaciones entre clases

---
+++ Back-end_1_IntroJAVA/Guia 11 - Relaciones/Ejercicios/Ejercicio2-PersonasPerros/src/relaciones_ejercicio2/Relaciones_Ejercicio2.java
@@ -0,0 +1,12 @@
+package relaciones_ejercicio2;
+
+/**
+ * @author Maximiliano Luna
+ */
+public class Relaciones_Ejercicio2 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
